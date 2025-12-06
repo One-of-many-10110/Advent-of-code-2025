a1c23f2e4fc444fdb988b2e62e700891928d7611
David A <david@otn.co.uk>
2025-12-06 10:53:05 +0000

    Day 5 done yay

--- advent of code day 5 part 2.py
+++ advent of code day 5 part 2.py
@@ -1,46 +1,57 @@
 # Day 5
 
-with open("example.txt",mode="r",encoding="utf-8-sig") as file:
+with open("input.txt",mode="r",encoding="utf-8-sig") as file:
     inputfile = file.read().split("\n")
     
 emptyindex = inputfile.index("") # grab the index of the separator of the ranges and foods
 ranges = inputfile[:emptyindex] # dont need the foods anymore
 
-# So this would be really easy with what i did before
-# so maybe see if in the ranges list the lowest index is inside the range of another
-# then if it is make that other ranges lower value and the first ranges higher value merge
-# that could work
+# New plan
+# sort the ranges so that the first value is ascending
+# then you can iterate through the ranges list until the ranges stop oerlapping in order to merge them
 
 def conglomeration(rangelist):
-    newlist = rangelist
-    previouslist = []
-    while newlist != previouslist:
-        previouslist = newlist[:]
-        for t, thing in enumerate(newlist):
-            for otherthing in newlist:
-                if otherthing[0] in range(thing[0],thing[1]) and otherthing != thing:
-                    newlist.remove(otherthing)
-                    if otherthing[1] > thing[1]:
-                        newlist.remove(thing)
-                        newlist.append([thing[0],otherthing[1]])
-                    break
-            if len(previouslist) != len(newlist):
-                break
+    # with the list sorted by the first value the lowest will always be ther first digit of the first range merged
+    # the highest you can simply check for each range
+    # the buffer range is made up of the lowest and current highest of the overlapping ranges
+    # so for each range you check if its highest value is within the range of the next or if it is higher than the next ranges highest
+    # once that does not happen anymore, the buffer range is added to the newlist and it starts a new buffer range
+    # if its at the end of the list it should stop and not try to compare itself to the next in the list
+    # could use a try statement for that
+    sortedlist = sorted(rangelist)
+    newlist = []
+    bufferrange = [0,0]
+    startofrange = True
+    for i, r in enumerate(sortedlist):
+        if startofrange == True:
+            bufferrange[0] = r[0]
+            startofrange = False
+        if r[1] > bufferrange[1]:
+            bufferrange[1] = r[1]
+        try:
+            nextrange = sortedlist[i+1]
+        except:
+            newlist.append(tuple(bufferrange))
+            bufferrange = [0,0]
+            startofrange = True
+        if startofrange == False:
+            if r[1] not in range(nextrange[0],nextrange[1]) and not r[1] >= nextrange[1]:
+                newlist.append(tuple(bufferrange))
+                bufferrange = [0,0]
+                startofrange = True
     return newlist
 
 
 
 for i,r in enumerate(ranges): # YAAY ENUMERATE
     newrange = list(map(int, r.split("-")))
-    newrange[1] += 1
     ranges[i] = newrange
 
 conglomeratedlist = conglomeration(ranges)
 validcount = 0
 
-for item in conglomeratedlist:
-    lower, higher = item
-    difference = higher - lower
+for lower, higher in conglomeratedlist:
+    difference = higher - lower + 1
     validcount += difference
 
 print(f"There are {validcount} fresh foods.")
