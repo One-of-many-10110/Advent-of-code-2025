dee991c4c423e97e9c4d9d2e0f2057262a8b0a7c
David A <david@otn.co.uk>
2025-12-11 20:30:50 +0000

    Day 11

--- advent of code day 11 part 2.py
+++ advent of code day 11 part 2.py
@@ -1,32 +1,34 @@
 # Day 11
 
+from functools import cache
+
 with open("input.txt",mode="r",encoding="utf-8-sig") as file:
     inputfile = file.read().split("\n") 
 
+@cache
 def routefinder(server,fft,dac):
-    global routecount
     outputs = servers[server]
     if outputs[0] == "out" and fft == True and dac == True:
-        routecount += 1
+        return 1
     elif outputs[0] == "out":
-        pass
+        return 0
     else:
+        returnvalue = 0
         for output in outputs:
             if output == "fft":
-                routefinder(output,True,dac)
+                returnvalue += routefinder(output,True,dac)
             elif output == "dac":
-                routefinder(output,fft,True)
+                returnvalue += routefinder(output,fft,True)
             else:
-                routefinder(output,fft,dac)
+                returnvalue += routefinder(output,fft,dac)
+        return returnvalue
 
 servers = {}
-routecount = 0
 
 for l, line in enumerate(inputfile):
     splitline = line.split(":")
     servers.update({splitline[0]:splitline[1].split()})
 
-routefinder("svr", False, False)
-print(f"The final route count is {routecount}.")
+print(f"The final route count is {routefinder("svr", False, False)}.")
 
 
